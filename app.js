32cc0f0ce11f412bb0423f9c49f81a4ae979d6d8
kadir-zeren <k4dzeren@gmail.com>
2024-03-21 10:03:38 +0300

    primitiv

---
+++ app.js
@@ -0,0 +1,67 @@
+// const arr = [100, "CW", false];
+// console.log(arr);
+
+// const dizi = new Array(100, "CW", true);
+// console.log(dizi);
+
+// const dizi2 = new Array(10);
+// console.log(dizi2);
+
+// const colors = ["Red", "Green", "Blue"];
+
+// console.log(colors[0]);
+// console.log(colors[colors.length - 1]);
+// console.log(colors[colors.length - 2]);
+// console.log(colors[-1]);
+// console.log(colors[56]);
+
+// const colors = ["Red", "Green", "Blue"];
+// console.log(typeof colors);
+
+// console.log(Array.isArray(colors));
+// console.log(colors instanceof Array);
+
+// const variables = "1";
+// console.log(Array.isArray(variables));
+// console.log(variables instanceof Array);
+
+// let str = "Brown";
+// console.log(str[0]);
+// str[0] = "b";
+// console.log(str);
+
+// const colors = ["Red", "Green", "Blue"];
+
+// colors[1] = "Yellow";
+
+// console.log(colors);
+
+// colors[3] = 5;
+
+// console.log(colors);
+
+// let c = "Pink";
+
+// colors[4] = c;
+
+// console.log(colors);
+
+const colors = ["Red", "Green", "Blue"];
+
+colors[6] = 2;
+
+console.log(colors);
+console.log(colors.length);
+console.log(colors[3]);
+
+colors[100] = "Pink";
+
+console.log(colors);
+
+let a = 5;
+let b = a;
+console.log(a);
+console.log(b);
+
+a = 6;
+console.log(b);
